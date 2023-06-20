6e8fe2d084bee9b870d500b78a4491fc41489fa0
JC <jcsacra4@gmail.com>
2023-06-20 14:57:38 +0800

    Fix export config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,253 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="relative max-w-screen overflow-x-hidden  overflow-y-hidden ">
+      <Image
+        src="/pattern-rings.svg"
+        alt="Github Icon"
+        width="530"
+        height="129"
+        className="absolute min-w-[530px] top-[124px] -left-[342px]"
+      />
+      <Image
+        src="/pattern-circle.svg"
+        alt="Github Icon"
+        width="130"
+        height="10"
+        className="absolute top-[254px] right-[-65px] overflow-hidden"
+      />
+      <header className="flex flex-col justify-between items-center w-[154px] m-auto  gap-5 relative">
+        <h1 className="text-name font-bold">adamkeyes</h1>
+        <section className="flex justify-between w-full">
+          <Image
+            src="/icon-github.svg"
+            alt="Github Icon"
+            width="24"
+            height="24"
+          />
+          <Image
+            src="/icon-frontend-mentor.svg"
+            alt="Frontend Mentor"
+            width="24"
+            height="22"
+          />
+          <Image
+            src="/icon-linkedin.svg"
+            alt="Linkedin"
+            width="24"
+            height="24"
+          />
+          <Image src="/icon-twitter.svg" alt="Twitter" width="23" height="18" />
+        </section>
+        <div className="w-[174px] h-[242px]">
+          <Image
+            src="/image-profile-mobile.webp"
+            alt="Profile Picture"
+            width="174"
+            height="242"
+            className="absolute top-0 left-0"
+          />
         </div>
-      </div>
+      </header>
+      <section className="w-[343px] m-auto flex flex-col text-center">
+        <h2 className="mt-10">
+          Nice to meet you! I&apos;m{" "}
+          <span className="underline-offset-8 decoration-4">Adam Keyes</span>
+        </h2>
+        <p className="mt-6">
+          Based in the UK, I&apos;m a front-end developer passionate about
+          building accessible web apps that users love.
+        </p>
+        <Link href="/" className="mx-auto mt-6">
+          CONTACT ME
+        </Link>
+      </section>
+      <section className="skillGrid ">
+        <section>
+          <section className="skill">HTML</section>
+          <p>4 Years Experience</p>
+        </section>
+        <section>
+          <section className="skill">CSS</section>
+          <p>4 Years Experience</p>
+        </section>
+        <section>
+          <section className="skill">Javascript</section>
+          <p>4 Years Experience</p>
+        </section>
+        <section>
+          <section className="skill">Accessibility</section>
+          <p>4 Years Experience</p>
+        </section>
+        <section>
+          <section className="skill">React</section>
+          <p>3 Years Experience</p>
+        </section>
+        <section>
+          <section className="skill">Sass</section>
+          <p>3 Years Experience</p>
+        </section>
+      </section>
+      <section className="projectSection">
+        <section className="projectHeader">
+          <h2>Projects</h2>
+          <Link href="/">CONTACT ME</Link>
+        </section>
+        <section className="projectList">
+          <article className="projectItem">
+            <section className="projectImage">
+              <Image
+                src="/thumbnail-project-1-large.webp"
+                alt="Twitter"
+                width="343"
+                height="253"
+              />
+            </section>
+            <section className="projectContent">
+              <h3 className="projectTitle">DESIGN PORTFOLIO</h3>
+              <section className="builtBy">
+                <p>HTML</p>
+                <p>CSS</p>
+              </section>
+            </section>
+            <section className="projectLinks">
+              <Link href="/" className="w-[138px]">
+                VIEW PROJECT
+              </Link>
+              <Link href="/" className="w-[103px]">
+                VIEW CODE
+              </Link>
+            </section>
+          </article>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <article className="projectItem">
+            <Image
+              src="/thumbnail-project-2-large.webp"
+              alt="Twitter"
+              width="343"
+              height="253"
+            />
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <section className="projectContent">
+              <h3 className="projectTitle">E-LEARNING LANDING PAGE</h3>
+              <section className="builtBy">
+                <p>HTML</p>
+                <p>CSS</p>
+              </section>
+            </section>
+            <section className="projectLinks">
+              <Link href="/" className="w-[138px]">
+                VIEW PROJECT
+              </Link>
+              <Link href="/" className="w-[103px]">
+                VIEW CODE
+              </Link>
+            </section>
+          </article>
+          <article className="projectItem">
+            <section className="projectImage">
+              <Image
+                src="/thumbnail-project-3-large.webp"
+                alt="Twitter"
+                width="343"
+                height="253"
+              />
+            </section>
+            <section className="projectContent">
+              <h3 className="projectTitle">TODO WEB APP</h3>
+              <section className="builtBy">
+                <p>HTML</p>
+                <p>CSS</p>
+              </section>
+            </section>
+            <section className="projectLinks">
+              <Link href="/" className="w-[138px]">
+                VIEW PROJECT
+              </Link>
+              <Link href="/" className="w-[103px]">
+                VIEW CODE
+              </Link>
+            </section>
+          </article>
+          <article className="projectItem">
+            <section className="projectImage">
+              <Image
+                src="/thumbnail-project-4-large.webp"
+                alt="Twitter"
+                width="343"
+                height="253"
+              />
+            </section>
+            <section className="projectContent">
+              <h3 className="projectTitle">ENTERTAINMENT WEB APP</h3>
+              <section className="builtBy">
+                <p>HTML</p>
+                <p>CSS</p>
+              </section>
+            </section>
+            <section className="projectLinks">
+              <Link href="/" className="w-[138px]">
+                VIEW PROJECT
+              </Link>
+              <Link href="/" className="w-[103px]">
+                VIEW CODE
+              </Link>
+            </section>
+          </article>
+          <article className="projectItem">
+            <section className="projectImage">
+              <Image
+                src="/thumbnail-project-5-large.webp"
+                alt="Twitter"
+                width="343"
+                height="253"
+              />
+            </section>
+            <section className="projectContent">
+              <h3 className="projectTitle">MEMORY GAME</h3>
+              <section className="builtBy">
+                <p>HTML</p>
+                <p>CSS</p>
+              </section>
+            </section>
+            <section className="projectLinks">
+              <Link href="/" className="w-[138px]">
+                VIEW PROJECT
+              </Link>
+              <Link href="/" className="w-[103px]">
+                VIEW CODE
+              </Link>
+            </section>
+          </article>
+          <article className="projectItem">
+            <section className="projectImage">
+              <Image
+                src="/thumbnail-project-6-large.webp"
+                alt="Twitter"
+                width="343"
+                height="253"
+              />
+            </section>
+            <section className="projectContent">
+              <h3 className="projectTitle">ART GALLERY SHOWCASE</h3>
+              <section className="builtBy">
+                <p>HTML</p>
+                <p>CSS</p>
+              </section>
+            </section>
+            <section className="projectLinks">
+              <Link href="/" className="w-[138px]">
+                VIEW PROJECT
+              </Link>
+              <Link href="/" className="w-[103px]">
+                VIEW CODE
+              </Link>
+            </section>
+          </article>
+        </section>
+      </section>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        space: ["var(--font-space-grotesk)", "sans-serif"],
       },
+      fontSize: {
+        name: ["24px", "40px"],
+        nav: ["40px", "40px"],
+        paragraph: ["16px", "26px"],
+        skills: ["32px", "40px"],
+        projectTitle: ["24px", "32px"],
+        builtBy: ["18px", "28px"],
+      },
+      gridTemplateRows: {
+        skills: "repeat(2,1fr)",
+        project: "repeat(2,1fr)",
+      },
+      gridTemplateColumns: {
+        skills: "repeat(auto-fit, minmax(300px, 1fr))",
+        project: "repeat(auto-fit, minmax(375px, 1fr))",
+      },
+      
     },
   },
   plugins: [],
-}
+};
