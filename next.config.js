8d87da60066918422858753e3b5098b6f16fb630
JC <jcsacra4@gmail.com>
2023-06-22 01:00:36 +0800

    Finish the mobile viewport for the project

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
  */
 const nextConfig = {
     output: 'export',
-    basePath:"/single-page-developer-portfolio"
+    assetPrefix:"/single-page-developer-portfolio"
     // Optional: Add a trailing slash to all paths `/about` -> `/about/`
     // trailingSlash: true,
     // Optional: Change the output directory `out` -> `dist`
